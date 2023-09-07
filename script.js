7b3696fb11b09286aa163607a6564b282ad5a338
JOAO VITOR <joaovitorbarros2000@gmai.com>
2023-09-07 10:03:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode () {
+    const html = document.documentElement
+  /*
+    if(html.classList.contains('claro')) {
+      html.classList.remove("claro")
+    } else {
+      html.classList.add("claro")
+    }
+    */
+   html.classList.toggle('claro')
+  }
